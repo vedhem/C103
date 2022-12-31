71411407d1d836503760048c6ffcfbdeead5f5a7
vedhem <96457962+vedhem@users.noreply.github.com>
2022-12-30 17:00:45 -0800

    Add files via upload

---
+++ PRO-C103-Student-Boilerplate-main/DownloadAndMove.py
@@ -0,0 +1,60 @@
+import sys
+import time
+import random
+
+import os
+import shutil
+
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+# from_dir = "ENTER THE PATH OF DOWNLOAD FOLDER (USE " / ") in VSC"
+# to_dir = "ENTER THE PATH OF DESTINATION FOLDER(USE " / ") in VSC"
+
+from_dir = "C:/Users/shema/Downloads"
+to_dir = "C:/Users/shema/OneDrive/Desktop/Document_Files"
+
+dir_tree = {
+    "Image_Files": ['.jpg', '.jpeg', '.png', '.gif', '.jfif'],
+    "Video_Files": ['.mpg', '.mp2', '.mpeg', '.mpe', '.mpv', '.mp4', '.m4p', '.m4v', '.avi', '.mov'],
+    "Documents_Files": ['.ppt', '.xls', '.csv', '.pdf', '.txt'],
+    "Setup_Files": ['.exe', '.bin', '.cmd', '.msi', '.dmg']
+}
+
+# Event Hanlder Class
+
+class FileMovementHandler(FileSystemEventHandler):
+    def on_created(self, event):
+        print(f"Hey, {event.src_path} has been created!")
+    def on_deleted(self,event):
+        print(f"Someone deleted {event.src_path}")
+    def on_modified(self,event):
+        print(f"Someone modified {event.src_path}")
+    def on_moved(self,event):
+        print(f"Someone moved {event.src_path}")
+        
+
+# Initialize Event Handler Class
+event_handler = FileMovementHandler()
+
+
+# Initialize Observer
+observer = Observer()
+
+# Schedule the Observer
+observer.schedule(event_handler, from_dir, recursive=True)
+
+
+# Start the Observer
+observer.start()
+
+try:
+    while True:
+        time.sleep(2)
+        print("running...")
+except KeyboardInterrupt:
+    print("stopped")
+    observer.stop()
+
+
+    
